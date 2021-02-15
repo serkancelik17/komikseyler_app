8e6e7dc2c59db483f864e2c4ef1907666913aef0
Serkan Celik <syaramaz@gmail.com>
2021-02-15 16:47:11 +0300

    lastview hitten ayrildi

--- lib/pages/views_page.dart
+++ lib/pages/views_page.dart
@@ -5,10 +5,9 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:komik_seyler/models/abstracts/section_abstract.dart';
 import 'package:komik_seyler/models/abstracts/view_abstract.dart';
-import 'package:komik_seyler/models/action.dart' as Local;
 import 'package:komik_seyler/models/ad.dart';
 import 'package:komik_seyler/partials/bottomBar.dart';
-import 'package:komik_seyler/repositories/picture_repository.dart';
+import 'package:komik_seyler/repositories/device_repository.dart';
 import 'package:komik_seyler/util/settings.dart';
 
 final mainScaffoldMessengerKey = GlobalKey<ScaffoldMessengerState>();
@@ -23,7 +22,7 @@ class ViewsPage extends StatefulWidget {
 }
 
 class _HomeState extends State<ViewsPage> {
-  PictureRepository _pictureRepository = new PictureRepository();
+  DeviceRepository _deviceRepository = new DeviceRepository();
   int pictureChangeCount = 0;
   List<ViewAbstract> views;
   int page = 1;
@@ -40,34 +39,31 @@ class _HomeState extends State<ViewsPage> {
   Widget build(BuildContext context) {
     print("section is " + widget.section.toString());
 
-    return WillPopScope(
-      onWillPop: _onBackPressed,
-      child: Scaffold(
-        appBar: Settings.buildAppBar(title: widget.section.getTitle()),
-        body: (views == null)
-            ? Center(child: CircularProgressIndicator())
-            : (views.length == 0)
-                ? Center(child: Text('Herhangi bir içerik bulunamadı.'))
-                : Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      CarouselSlider(
-                        options: CarouselOptions(
-                          height: MediaQuery.of(context).size.height * 0.6,
-                          onPageChanged: pageChange,
-                          enlargeCenterPage: true,
-                          enableInfiniteScroll: false,
-                          viewportFraction: 0.8,
-                        ),
-                        items: views.map((view) {
-                          return buildBuilder(view);
-                        }).toList(),
+    return Scaffold(
+      appBar: Settings.buildAppBar(title: widget.section.getTitle()),
+      body: (views == null)
+          ? Center(child: CircularProgressIndicator())
+          : (views.length == 0)
+              ? Center(child: Text('Herhangi bir içerik bulunamadı.'))
+              : Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    CarouselSlider(
+                      options: CarouselOptions(
+                        height: MediaQuery.of(context).size.height * 0.6,
+                        onPageChanged: pageChange,
+                        enlargeCenterPage: true,
+                        enableInfiniteScroll: false,
+                        viewportFraction: 0.8,
                       ),
-                      (activeView is Ad) ? SizedBox(width: 1) : Settings.getBannerAd(),
-                    ],
-                  ),
-        bottomNavigationBar: (activeView is Ad) ? null : BottomBar(context: context, currentView: activeView),
-      ),
+                      items: views.map((view) {
+                        return buildBuilder(view);
+                      }).toList(),
+                    ),
+                    (activeView is Ad) ? SizedBox(width: 1) : Settings.getBannerAd(),
+                  ],
+                ),
+      bottomNavigationBar: (activeView is Ad) ? null : BottomBar(context: context, currentView: activeView),
     );
   }
 
@@ -109,17 +105,9 @@ class _HomeState extends State<ViewsPage> {
     setState(() {
       activeView = views[index];
     });
-    if (index == views.length - 2) getMore();
-  }
+    //Update lastView
+    _deviceRepository.updateLastView(picture: activeView);
 
-  Future<bool> _onBackPressed() async {
-    // if last view is Ad
-    if (activeView is Ad) {
-      views.removeLast();
-      activeView = views.last;
-    }
-    //Add hit
-    _pictureRepository.addAction(action: new Local.Action(id: 3, name: 'hit'), picture: activeView);
-    return true;
+    if (index == views.length - 2) getMore();
   }
 }
--- lib/repositories/device_repository.dart
+++ lib/repositories/device_repository.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:komik_seyler/models/device.dart';
+import 'package:komik_seyler/models/picture.dart';
+import 'package:komik_seyler/models/response.dart';
 import 'package:komik_seyler/providers/api_provider.dart';
+import 'package:komik_seyler/util/settings.dart';
 
 class DeviceRepository {
   ApiProvider provider;
@@ -19,5 +22,17 @@ class DeviceRepository {
     String endpoint = '/devices/' + uuid + '/store';
     String apiResponse = await provider.getResponse(endpoint);
     return deviceFromJson(apiResponse);
+  }
+
+  Future<Response> updateLastView({@required Picture picture}) async {
+    Device _device = await Settings.getDevice();
+    String endpoint = '/devices/' + _device.id.toString() + '/update_last_view/' + picture.id.toString();
+    try {
+      Response response = responseFromJson(await provider.getResponse(endpoint));
+
+      return response;
+    } catch (e) {
+      throw e;
+    }
   }
 }
