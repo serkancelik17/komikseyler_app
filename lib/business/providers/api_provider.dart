3f0cb41e1411398de3f7d874802054da90d36e71
Serkan Celik <syaramaz@gmail.com>
2021-02-27 23:12:35 +0300

    KS-3 #time 3h #comment cache custom edildi.

--- lib/business/providers/api_provider.dart
+++ lib/business/providers/api_provider.dart
@@ -1,23 +1,30 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:http/http.dart' as http;
 import 'package:komik_seyler/business/config/env.dart';
 
 class ApiProvider {
   String _apiUrl = Env.baseUrl + "/api/v1";
+  DefaultCacheManager defaultCacheManager;
 
-  ApiProvider();
+  ApiProvider({this.defaultCacheManager}) {
+    this.defaultCacheManager ??= DefaultCacheManager();
+  }
 
   ///veriyi getir
   Future<String> get(String endpoint) async {
     final url = _apiUrl + endpoint;
     debugPrint("[GET] Request Url : " + url);
     try {
-      http.Response responseRaw = await http.get(url);
-
-      if (responseRaw.statusCode == 200) {
-        return responseRaw.body;
+      // http.Response responseRaw = await http.get(url);
+      File response = await defaultCacheManager.getSingleFile(url);
+      print("[GET CACHED] : " + response.absolute.toString() + " lastModified: " + (await response.lastModified()).toString());
+      if (await response.exists()) {
+        return response.readAsString();
       } else {
-        throw Exception("GET Problem!. (Status code is: " + responseRaw.statusCode.toString() + ")");
+        throw Exception("GET Problem!.");
       }
     } catch (e) {
       rethrow;
